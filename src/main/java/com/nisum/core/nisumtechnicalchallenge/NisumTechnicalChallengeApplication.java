3e64f4392b6e09a044731844d60925460cdd71d6
cristian <cristianp@ecsfin.com>
2024-05-16 19:11:55 -0500

    upload base project

---
+++ src/main/java/com/nisum/core/nisumtechnicalchallenge/NisumTechnicalChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.core.nisumtechnicalchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumTechnicalChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NisumTechnicalChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nisum/core/nisumtechnicalchallenge/NisumTechnicalChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.core.nisumtechnicalchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisumTechnicalChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
